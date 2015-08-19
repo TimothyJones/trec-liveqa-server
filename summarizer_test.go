8f2032468e0b1a43ebdbc219dd81b5c286aa5e82
Ruey-Cheng Chen <rueycheng@gmail.com>
2015-08-20 00:21:09 +1000

    Fix summarizer_test.go

--- summarizer_test.go
+++ summarizer_test.go
@@ -6,9 +6,9 @@ import (
 )
 
 func TestPayload(t *testing.T) {
-	ss := StandardSummarizer{"localhost:8001"}
+	ss := RemoteSummarizer{"localhost:8001"}
 
-	doc := &Document{"Trec LiveQA Trac: 2015 The automated question answering (QA) track, which has been one of the most popular tracks in TREC for many years, has focused on the task of providing automatic answers for human questions."}
+	doc := &Document{Text: "Trec LiveQA Trac: 2015 The automated question answering (QA) track, which has been one of the most popular tracks in TREC for many years, has focused on the task of providing automatic answers for human questions."}
 
 	q := &Question{
 		Qid:          "fake-qid",
@@ -18,9 +18,10 @@ func TestPayload(t *testing.T) {
 		ReceivedTime: time.Unix(0, 0),
 	}
 
-	expected := `{"document":{"Text":"Trec LiveQA Trac: 2015 The automated question answering (QA) track, which has been one of the most popular tracks in TREC for many years, has focused on the task of providing automatic answers for human questions."},"question":{"qid":"fake-qid","title":"What is artificial intelligence?","body":"history and evolution of artificial intelligence","category":"Programming \u0026 Design","ReceivedTime":"1970-01-01T10:00:00+10:00"},"limit":250}`
+	expected := `{"documents":[{"docno":"","text":"Trec LiveQA Trac: 2015 The automated question answering (QA) track, which has been one of the most popular tracks in TREC for many years, has focused on the task of providing automatic answers for human questions.","title":"","url":""}],"question":{"qid":"fake-qid","title":"What is artificial intelligence?","body":"history and evolution of artificial intelligence","category":"Programming \u0026 Design","ReceivedTime":"1970-01-01T10:00:00+10:00"},"limit":250}`
 
-	payload := ss.GetPayload(doc, q, 250)
+	docs := []Document{*doc}
+	payload := ss.GetPayload(docs, q, 250)
 	if string(payload) != expected {
 		t.Errorf("Expected payload `%s` but got `%s`", expected, string(payload))
 	}
