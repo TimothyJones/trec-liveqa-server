0b86ba58e4c678ad432058bd52f8ece1e035855f
Timothy Jones <timothy.l.jones@gmail.com>
2015-08-28 14:41:53 +1000

    Added server location to debug

--- headwords.go
+++ headwords.go
@@ -21,7 +21,7 @@ func GetHeadWord(question string) chan string {
 
 		if _, ok := r["result"]; !ok {
 			// We didn't receive a result
-			log.Printf("Headword: question '%s' had no result from the server (%s, %T)\n", r, r)
+			log.Printf("Headword: HeadWord at '%s' question '%s' had no result from the server (%s, %T)\n", config.HeadWordServer, question, r, r)
 			c <- ""
 			return
 		}
