37a58e73006da3fc10e55872d02be814bc9216f7
Timothy Jones <timothy.l.jones@gmail.com>
2015-08-28 15:14:42 +1000

    Added word2vec code

--- word2vec.go
+++ word2vec.go
@@ -6,26 +6,31 @@ import (
 	"strings"
 )
 
-func word2vec(str string) string {
+func word2vec(word string) string {
 	args := make([]interface{}, 1)
-	args[0] = str
+	args[0] = word
 
 	r, err := httpjsonrpc.Call(config.Word2VecServer, "expand", 0, args)
 	if err != nil {
-		log.Printf("Unable to get word2vec expansion for '%s'\n", str)
-		return ""
+		log.Printf("verbose error info: %#v", err)
 	}
 
 	if _, ok := r["result"]; !ok {
+		log.Println("Word2Vec: not getting any result")
 		return ""
 	}
 
-	x, ok := r["result"].([]string)
-	if ok {
-		return strings.Join(x, " ")
+	x, ok := r["result"].([]interface{})
+	if !ok {
+		log.Println("Word2Vec: cannot parse the result")
+		return ""
 	} else {
-		return ""
+		strs := make([]string, len(x))
+		for i, item := range x {
+			strs[i] = item.(string)
+		}
+		result := strings.Join(strs, " ")
+		log.Printf("Word2Vec expanded '%s' to '%s'\n", word, result)
+		return result
 	}
-
-	return str
 }
